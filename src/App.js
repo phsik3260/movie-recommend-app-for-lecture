1b50e260db9cf57a1215d5ae594f8e5b0fabf1e3
hyunsik Park <phsik3260@gmail.com>
2022-09-21 12:33:39 +0900

    ~#5.1 Tour of CRA

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App() {
+import Button from "./Button";
+import styles from "./App.module.css";
+
+const App = () => {
   return (
     <>
-      <h1>hello world</h1>
+      <h1 className={styles.title}>hello world</h1>
+      <Button text={"Continue"} />
     </>
   );
-}
+};
 
 export default App;
