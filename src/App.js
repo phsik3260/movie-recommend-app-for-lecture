fa921ecfa262bcefe81d34692af1def914dc1ece
hyunsik Park <phsik3260@gmail.com>
2022-09-22 10:39:56 +0900

    ~#6.2 Deps 1/2

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
 
   const onClick = () => setCounter((curr) => curr + 1);
 
+  const changeKeyword = (event) => setKeyword(event.target.value);
+
   useEffect(() => console.log("call api"), []);
-  // useEffect는 첫 번째 인자인 function이, 앱이 실행될 때 딱 한 번만 호출될 수 있도록 보호해주는 역할을 한다.
-  // 즉 Component가 rerendering될 때 useEffect를 통해 특정 코드의 호출을 제한할 수 있다.
+
+  useEffect(() => console.log(`search for: ${keyword}`), [keyword]);
+  // useEffect의 두 번째 인자인 의존성 배열에 변수를 할당하면, 변수의 값이 변화할 때마다 useEffect의 첫 번째 인자인 function이 호출된다.
+  // 즉 특정한 변수의 값이 바뀔 때마다 원하는 함수를 호출시킬 수 있다.
 
   return (
     <>
       <h3>Total clicks: {counter}</h3>
       <button onClick={onClick}>Click me</button>
+      <br />
+      <br />
+      <br />
+      <div>
+        <label htmlFor="">Movie Search</label>
+        <input type="text" value={keyword} onChange={changeKeyword} />
+      </div>
     </>
   );
 };
