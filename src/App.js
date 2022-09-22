a078da81d9adc1199988e1c04a18755cc14dd372
hyunsik Park <phsik3260@gmail.com>
2022-09-22 10:11:46 +0900

    ~#6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
 
   const onClick = () => setCounter((curr) => curr + 1);
 
-  console.log("call api");
-  // setState를 통해 state의 값을 변경하면, 해당 Component 전체가 rerendering된다.
-  // 따라서 위의 코드또한 rerendering되어 불필요하게 api를 다시 호출하게 된다.
-  // App이 실행될 때 처음 한 번만 rendering되고, 이후에 (state의 값이 변경되더라도) rerendering되기를 원하지 않는 코드는 어떻게 처리할 수 있을까?
+  useEffect(() => console.log("call api"), []);
+  // useEffect는 첫 번째 인자인 function이, 앱이 실행될 때 딱 한 번만 호출될 수 있도록 보호해주는 역할을 한다.
+  // 즉 Component가 rerendering될 때 useEffect를 통해 특정 코드의 호출을 제한할 수 있다.
 
   return (
     <>
