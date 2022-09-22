a4828817511d98bdb498376f6168492631c2ecc0
hyunsik Park <phsik3260@gmail.com>
2022-09-22 11:52:08 +0900

    ~#6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -1,53 +1,24 @@
 import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
 
-  const onClick = () => setCounter((curr) => curr + 1);
-
-  const changeKeyword = (event) => setKeyword(event.target.value);
-
-  console.log("I run all time.");
-
-  useEffect(() => console.log("I run only once."), []);
-  useEffect(() => {
-    if (counter === 0) {
-      return;
-    }
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    if (keyword === "") {
-      return;
-    }
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    if (counter === 0 || keyword === "") {
-      return;
-    }
-    console.log("I run when 'counter & keyword' changes.");
-  }, [counter, keyword]);
+  const onClick = () => setShowing((curr) => !curr);
 
   return (
     <>
-      <h3>Total clicks: {counter}</h3>
-      <button onClick={onClick}>Click me</button>
-      <br />
-      <br />
-      <br />
-      <div>
-        <label htmlFor="">Movie Search</label>
-        <input
-          type="text"
-          placeholder="Search here..."
-          value={keyword}
-          onChange={changeKeyword}
-        />
-      </div>
+      {showing && <Hello />}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </>
   );
+};
+const Hello = () => {
+  useEffect(() => {
+    console.log("I'm here!");
+    return () => console.log("destroyed!"); // cleanUp function: 해당 Component가 destroy될 때 호출된다.
+  }, []);
+
+  return <h1>Hello</h1>;
 };
 
 export default App;
