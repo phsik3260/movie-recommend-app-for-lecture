6ede7ad705c88ec53e22be5d1cd6e79d94e0684f
hyunsik Park <phsik3260@gmail.com>
2022-09-23 13:46:01 +0900

    ~#7.2 Coin Tracker Challenge

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from "react";
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [dollars, setDollars] = useState("");
+  const [index, setIndex] = useState("0");
+  const [price, setPrice] = useState(0);
 
   useEffect(() => {
     const url = "https://api.coinpaprika.com/v1/tickers";
@@ -14,21 +17,59 @@ const App = () => {
     );
   }, []);
 
+  const changeDollars = (event) => setDollars(event.target.value);
+  const changeIndex = (event) => {
+    setIndex(event.target.value);
+    setPrice(coins[event.target.value - 1].quotes.USD.price);
+  };
+
   return (
     <>
       {loading ? (
         <span>get loading...</span>
       ) : (
         <div>
-          <h1>The Coins: {coins.length}</h1>
-          <ul>
+          <h1>Total Coins: {coins.length}</h1>
+          <select value={index} onChange={changeIndex}>
+            <option value={"0"}>### SELECT YOUR COIN ###</option>
             {coins.map((coin) => (
-              <li key={coin.id}>
+              <option key={coin.id} value={coin.rank}>
                 {coin.name} ({coin.symbol}):{" "}
                 {Math.round(coin.quotes.USD.price * 10) / 10}$
-              </li>
+              </option>
             ))}
-          </ul>
+          </select>
+          {index != 0 && (
+            <div>
+              <hr />
+              <form>
+                <label htmlFor="dollars-input">
+                  How many dollars you have?
+                </label>
+                <input
+                  id="dollars-input"
+                  type="number"
+                  placeholder="Enter here..."
+                  min={0}
+                  value={dollars}
+                  onChange={changeDollars}
+                />
+              </form>
+              {dollars !== "" && dollars !== "0" && (
+                <div>
+                  <div>
+                    현재 <strong>{dollars}$</strong> 보유 중입니다.
+                  </div>
+                  <div>
+                    이 코인의 가격은 <strong>{price}$</strong> 입니다.
+                  </div>
+                  <div>
+                    당신은 <strong>{dollars / price}개</strong> 살 수 있습니다.
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       )}
     </>
