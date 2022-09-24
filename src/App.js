e9e9ca9771b909ad8be4b341849146493c93a071
hyunsik Park <phsik3260@gmail.com>
2022-09-24 16:37:20 +0900

    ~#7.3 Movie App part One

--- src/App.js
+++ src/App.js
@@ -2,74 +2,44 @@ import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [dollars, setDollars] = useState("");
-  const [index, setIndex] = useState("0");
-  const [price, setPrice] = useState(0);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const url =
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year";
+    const json = await (await fetch(url)).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  console.log(movies);
 
   useEffect(() => {
-    const url = "https://api.coinpaprika.com/v1/tickers";
-    fetch(url).then((response) =>
-      response.json().then((json) => {
-        setCoins(json);
-        setLoading(false);
-      })
-    );
+    getMovies();
   }, []);
-
-  const changeDollars = (event) => setDollars(event.target.value);
-  const changeIndex = (event) => {
-    setIndex(event.target.value);
-    setPrice(coins[event.target.value - 1].quotes.USD.price);
-  };
 
   return (
     <>
       {loading ? (
-        <span>get loading...</span>
+        "get loading..."
       ) : (
         <div>
-          <h1>Total Coins: {coins.length}</h1>
-          <select value={index} onChange={changeIndex}>
-            <option value={"0"}>### SELECT YOUR COIN ###</option>
-            {coins.map((coin) => (
-              <option key={coin.id} value={coin.rank}>
-                {coin.name} ({coin.symbol}):{" "}
-                {Math.round(coin.quotes.USD.price * 10) / 10}$
-              </option>
-            ))}
-          </select>
-          {index != 0 && (
-            <div>
-              <hr />
-              <form>
-                <label htmlFor="dollars-input">
-                  How many dollars you have?
-                </label>
-                <input
-                  id="dollars-input"
-                  type="number"
-                  placeholder="Enter here..."
-                  min={0}
-                  value={dollars}
-                  onChange={changeDollars}
-                />
-              </form>
-              {dollars !== "" && dollars !== "0" && (
-                <div>
-                  <div>
-                    현재 <strong>{dollars}$</strong> 보유 중입니다.
-                  </div>
-                  <div>
-                    이 코인의 가격은 <strong>{price}$</strong> 입니다.
-                  </div>
-                  <div>
-                    당신은 <strong>{dollars / price}개</strong> 살 수 있습니다.
-                  </div>
-                </div>
-              )}
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} alt={movie.title} />
+              <div>
+                <h2>{movie.title}</h2>
+                <div>{movie.year}</div>
+                <div>{movie.rating}</div>
+                <ul>
+                  {movie.genres.map((genre) => (
+                    <li key={genre}>{genre}</li>
+                  ))}
+                </ul>
+                <div>{movie.summary}</div>
+              </div>
             </div>
-          )}
+          ))}
         </div>
       )}
     </>
